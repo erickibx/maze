03f27bb5c7fc52ad48faf726fca0a6f027845f0f
Erick Arturo Ibañez <36176934+erickibx@users.noreply.github.com>
2023-08-01 22:29:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -526,7 +526,7 @@ window.onload = function() {
   };
   sprite = new Image();
   sprite.src =
-    "https://image.ibb.co/dr1HZy/Pf_RWr3_X_Imgur.png" +
+    "https://i.ibb.co/1swJhMQ/dot.png" +
     "?" +
     new Date().getTime();
   sprite.setAttribute("crossOrigin", " ");
@@ -538,7 +538,7 @@ window.onload = function() {
   };
 
   finishSprite = new Image();
-  finishSprite.src = "https://image.ibb.co/b9wqnJ/i_Q7m_U25_Imgur.png"+
+  finishSprite.src = "https://i.ibb.co/kht8bgC/flag.png"+
   "?" +
   new Date().getTime();
   finishSprite.setAttribute("crossOrigin", " ");
